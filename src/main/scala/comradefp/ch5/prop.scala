8b4cdd3584df52cb1a2dd2f515d108361e670567
bigknife <bigknife@outlook.com>
2017-11-02 23:01:30 +0800

    add prop

--- src/main/scala/comradefp/ch5/prop.scala
+++ src/main/scala/comradefp/ch5/prop.scala
@@ -1,9 +1,10 @@
 package prop
 
 import gen._
+import rng._
 import Prop._
 
-sealed trait Prop
+case class Prop(run: (TestCases, RNG) => Result)
 
 object Prop {
 
@@ -16,5 +17,21 @@ object Prop {
   type TestCases = Int
   type Result = Either[FailedCase, (Status, SuccessCount)]
 
-  def forAll[A](a: Gen[A])(f: A => Boolean): Prop = ???
+  def forAll[A](a: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) =>
+    @annotation.tailrec
+    def go(i: Int, n: Int, x: Option[Result]): Result = {
+      if (i == n) Right((Proven, i))
+      else if (x.isDefined) x.get
+      else {
+        val x = a.sample.run(rng)
+        val r: Either[String, Boolean] = try {Right(f(x._1))}catch{case e: Throwable => Left(e.getMessage)}
+        r match {
+          case Left(msg) => go(i + 1, n, Some(Left(msg)))
+          case Right(true) => go(i + 1, n, None)
+          case Right(false) => go(i + 1, n, Some(Left(x._1.toString())))
+        }
+       }
+    }
+    go(0, n, None)
+  }
 }
